85531f2842ed083eddad26f6abf2879c9abf55db
tifa <latifaprses@outlook.com>
2025-02-02 20:40:35 +0100

    fixed the problem with guessing number not changing after resetting the game'

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 'use strict';
-const number = Math.trunc(Math.random() * 20 + 1);
+
+let number = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
 
-const displayMessage = function(message){
+const broj = document.querySelector('.number');
+broj.textContent = number;
+
+const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
-}
+};
+
 document.querySelector('.score').textContent = score;
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  let guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
   if (!guess) {
@@ -19,30 +24,31 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = number;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = number;
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
-    } }else if (guess != number){
-      if (score > 1) {
-        displayMessage(guess > number ? 'Too high!📈' : 'Too low 📉') ;
-        score--;
-        document.querySelector('.score').textContent = score;
-      } else {
-        displayMessage('You lost the game 🤯');
-        document.querySelector('.score').textContent = 0;
-      }
     }
-  
+  } else if (guess !== number) {
+    if (score > 1) {
+      displayMessage(guess > number ? 'Too high!📈' : 'Too low 📉');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game 🤯');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
-  const number = Math.trunc(Math.random() * 20 + 1);
-  let score = 20;
+const newGame = function () {
+  number = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').textContent = '';
-  displayMessage('Start guessing...');
+  document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.score').textContent = score;
-});
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+};
+
+document.querySelector('.again').addEventListener('click', newGame);
